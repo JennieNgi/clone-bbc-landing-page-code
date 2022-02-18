541fe162292b32a6e23ddb7f5cea710d050c7e03
Jenny Ngi <75710628+JennieNgi@users.noreply.github.com>
2022-02-18 18:33:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,96 @@
-// importing the sass stylesheet for bundling
-import "./../sass/styles.scss";
-import "./../node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scss";
-import "./../node_modules/@fortawesome/fontawesome-free/scss/solid.scss";
 
-// JS content goes here
-// ...
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+/*==================== MOBILE MENU SHOW ====================*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close');
 
-// Select tab content item
-function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab item
-	this.classList.add('tab-border');
-	// Grab content item from DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	// Add show class
-	tabContentItem.classList.add('show');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
 }
 
-// Remove bottom borders from all tab items
-function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
 }
 
-// Remove show class from all content items
-function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
+
+/*==================== SKILLS ====================*/
+const skillsFrontend = document.getElementById('skills_frontend');
+const skillsBackend = document.getElementById('skills_backend');
+const skillsOther = document.getElementById('skills_other');
+const frontendArrow = document.getElementById('frontend_arrow');
+const backendArrow = document.getElementById('backend_arrow');
+const otherArrow = document.getElementById('other_arrow');
+
+if(frontendArrow){
+  frontendArrow.addEventListener('click', () => {
+      skillsFrontend.classList.add('skills_open');
+      skillsBackend.classList.remove('skills_open'); 
+      skillsOther.classList.remove('skills_open'); 
+  });
 }
 
-// Listen for tab item click
-tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
+
+if(backendArrow){
+  backendArrow.addEventListener('click', () => {
+    skillsBackend.classList.add('skills_open');
+    skillsFrontend.classList.remove('skills_open');
+    skillsOther.classList.remove('skills_open');
+  });
+}
+
+if(otherArrow){
+  otherArrow.addEventListener('click', () => {
+    skillsOther.classList.add('skills_open');
+    skillsBackend.classList.remove('skills_open');
+    skillsFrontend.classList.remove('skills_open');
+  });
+}
+      
+      
+/*==================== QUALIFICATION ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll ('[data-content]');
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active');
+        });
+        target.classList.add('qualification_active');
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active');
+        });
+        tab.classList.add('qualification_active');
+    });
+
 });
+
+
+
+//active nav link
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".nav_menu ul li a");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
